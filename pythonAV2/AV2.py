ee6b1a59a5fc6b92473c57aeb8cf457467fec11e
cleysonseveriano <cleyson.tab@gmail.com>
2022-11-21 19:25:48 -0300

    commiting

--- pythonAV2/AV2.py
+++ pythonAV2/AV2.py
@@ -6,3 +6,4 @@ print('Lista de números: ',numeros)
 random.shuffle(numeros) # Embaralha os números
 print('\nLista embaralhada: ',numeros)
 print('\nResultado: ', sorted(numeros[0:6]))# Por fim pegamos os últimos 6 números
+pass
